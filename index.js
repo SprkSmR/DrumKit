16d20008f678923b3e9567a7c20732b27089b149
SprkSmR <chitox_ji_armen@hotmail.com>
2025-08-16 02:51:16 -0600

    How to Play Sounds on a Website + Switch Statements - Done

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-var audio = new Audio("./sounds/tom-1.mp3");
+var audio = new Audio();
 
 document.querySelectorAll("button").forEach(button => {
     button.addEventListener("click", function (){
-        audio.play();   
+        this.classList.add("pressed");
+
+        switch(this.innerHTML){
+            case "w":
+                audio.src = "./sounds/tom-1.mp3";
+                break;
+            case "a":
+                audio.src = "./sounds/tom-2.mp3";
+                break;
+            case "s":
+                audio.src = "./sounds/tom-3.mp3";
+                break;
+            case "d":
+                audio.src = "./sounds/tom-4.mp3";
+                break;
+            case "j":
+                audio.src = "./sounds/snare.mp3";
+                break;
+            case "k":
+                audio.src = "./sounds/crash.mp3";
+                break;
+            case "l":
+                audio.src = "./sounds/kick-bass.mp3";
+                break;
+            default:
+        }
+
+        audio.play();
     });
 });
+
 
